637b7555048caee4f53eae3062468b21c1975074
meps106130 <120775416+meps106130@users.noreply.github.com>
2025-10-06 14:24:34 +0800

    Delete miniblox-website directory

--- miniblox-website/index/script.js
+++
@@ -1,9 +0,0 @@
-// 當點擊語言按鈕時，跳轉到另一語言頁面
-document.getElementById("langBtn").addEventListener("click", function() {
-  const currentURL = window.location.href;
-  if (currentURL.includes("-zh.html")) {
-    window.location.href = currentURL.replace("-zh.html", "-en.html");
-  } else if (currentURL.includes("-en.html")) {
-    window.location.href = currentURL.replace("-en.html", "-zh.html");
-  }
-});
