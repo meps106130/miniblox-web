52a4911ca5a32255a03ab502d2b7e3fabb322c5d
meps106130 <120775416+meps106130@users.noreply.github.com>
2025-10-07 12:51:28 +0800

    Delete miniblox-website directory

--- miniblox-website/development/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("langBtn").addEventListener("click", () => {
-  const current = document.documentElement.lang;
-  if (current === "zh") {
-    window.location.href = "index-en.html";
-  } else {
-    window.location.href = "index-zh.html";
-  }
-});
--- miniblox-website/keyfigures/script.js
+++
@@ -1,2 +0,0 @@
-// 暫時不需額外互動
-console.log("Key Figures page loaded.");
