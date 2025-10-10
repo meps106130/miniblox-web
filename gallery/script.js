c28d1cf25ba53ac24faeb5f13032d6674206c0b8
meps106130 <120775416+meps106130@users.noreply.github.com>
2025-10-10 18:19:01 +0800

    Add files via upload

--- gallery/script.js
+++ gallery/script.js
@@ -13,3 +13,23 @@ thumbs.forEach(img => {
 closeBtn.addEventListener("click", () => {
   lightbox.classList.add("hidden");
 });
+
+
+document.querySelectorAll('.card').forEach(card => {
+  const back = card.querySelector('.card-back');
+  if (!back) return;
+  const text = back.innerText.toLowerCase();
+  if (text.includes('lvl: 100')) {
+    card.classList.add('level-100'); // 紅色動畫
+  } else if (text.includes('rank: pro')) {
+    card.classList.add('rank-pro');
+  } else if (text.includes('rank: legend')) {
+    card.classList.add('rank-legend');
+  } else if (text.includes('rank: immortal')) {
+    card.classList.add('rank-immortal');
+  } else if (text.includes('rank: bulider')) {
+    card.classList.add('rank-bulider');
+  } else if (text.includes('rank: og')) {
+    card.classList.add('rank-bulider');
+  }
+});
